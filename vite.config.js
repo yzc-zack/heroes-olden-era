205ebd6aeb109480fb9db03c00b169fcebf44789
zhicheng.yao <zhicheng.yao@enesource.com>
2026-05-08 09:45:39 +0800

    修改目录

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
+  base: "/docs/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
